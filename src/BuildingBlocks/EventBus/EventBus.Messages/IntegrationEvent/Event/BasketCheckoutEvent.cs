f8b71cc1f4358d95146e525859cd00de7aa4d7f4
Phan Vu <anhvu2182004@gmail.com>
2025-05-10 10:04:38 +0700

    [ADD]: Add checkout service

---
+++ src/BuildingBlocks/EventBus/EventBus.Messages/IntegrationEvent/Event/BasketCheckoutEvent.cs
@@ -0,0 +1,17 @@
+using EventBus.Messages.IntegrationEvent.Interface;
+
+namespace EventBus.Messages.IntegrationEvent.Event;
+
+public record BasketCheckoutEvent : IntegrationBaseEvent, IBasketCheckoutEvent
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Address { get; set; } = null!;
+    public string PhoneNumber { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public int TotalPrice { get; set; }
+    public int ShippingFee { get; set; }
+    public int PaymentMethod { get; set; }
+    public Guid UserId { get; set; }
+    public List<BasketCheckoutEventItem> Items { get; set; } = [];
+}
